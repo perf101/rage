aefb892ef96771a591481376402c8575ed28828e
Rok Strniša <rok.strnisa@citrix.com>
2012-07-18 17:07:53 +0000

    Fixed a small bug where tooltip were appearing in a wrong place.
    Signed-off-by: Rok Strniša <rok.strnisa@citrix.com>

--- static/rage.js
+++ static/rage.js
@@ -594,11 +594,11 @@ function create_log_ticks(axis) {
 }
 
 function show_tooltip(graph, page_x, page_y, contents) {
-  var graph_pos = graph.position();
+  var graph_pos = graph.offset();
   var x = page_x - graph_pos.left;
   var y = page_y - graph_pos.top;
   var tooltip = $(contents).css({
-    'top': y - 20, 'left': x + 5
+    'top': y, 'left': x + 5
   }).appendTo(graph).fadeIn(200);
   tooltip.children("img").click(function () {
     $(this).parent().remove();
