833b250ec0ad52454d662e4687a4bbfb9c4956c4
Jonathan Davies <jonathan.davies@citrix.com>
2015-04-23 15:48:24 +0100

    import_jobs_handler: allow lists of jobs

--- src/import_jobs_handler.ml
+++ src/import_jobs_handler.ml
@@ -8,10 +8,15 @@ let importer = "/usr/groups/perfeng/bin/importer-xenrt"
  * We accept either:
  *  - jobid from import_page form
  *  - JOBID from XenRT CALLBACK_URL
+ *
+ * Value can be either '<n>' or '<n0>-<n1>' or '<n0>,<n1>,...'.
  *)
 
-let import_job job_id =
-  let cmd = Printf.sprintf "%s -job %d -ignoreseenjobs" importer job_id in
+let import_job job_ids =
+  (* Sanitise input *)
+  if not (Str.string_match (Str.regexp "^[0-9,\\-]*$") job_ids 0) then failwith (sprintf "expected '&lt;n&gt;' or '&lt;n&gt;-&lt;n&gt;' or '&lt;n&gt;,&lt;n&gt;,...'; got '%s'" job_ids);
+
+  let cmd = Printf.sprintf "%s -jobs %s -ignoreseenjobs" importer job_ids in
   let ic = Unix.open_process_in cmd in
   try
     while true do
@@ -24,15 +29,15 @@ let t ~args = object (self)
   inherit Html_handler.t ~args
 
   method private write_body =
-    let job_id = try
+    let job_ids = try
         self#get_param_exn "jobid"
       with Not_found ->
         self#get_param_exn "JOBID"
     in
-    let job_id = int_of_string job_id in
-    Printf.printf "Processing job %d..." job_id;
+    let job_ids = decode_html job_ids in
+    Printf.printf "Processing job(s) %s..." job_ids;
     Printf.printf "<pre>";
-    import_job job_id;
+    import_job job_ids;
     Printf.printf "</pre>";
     Printf.printf "Finished."
 
