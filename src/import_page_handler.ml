52321a94ee646bea33b943122f24a719855e9149
Joshua Perrett <joshua.perrett@gmail.com>
2017-07-26 16:55:18 +0100

    improved appearance of import job page

--- src/import_page_handler.ml
+++ src/import_page_handler.ml
@@ -4,6 +4,7 @@ let t ~args = object (self)
   inherit Html_handler.t ~args
 
   method private write_body =
+    printf "<div class=\"import_page\">\n";
     printf "<p>Data from completed XenRT jobs is imported automatically daily.</p>\n";
     printf "<p>This page can be used to force the immediate import of one or more XenRT jobs.</p>\n";
     printf "<form action='/' method='post'>\n";
@@ -12,5 +13,6 @@ let t ~args = object (self)
     printf "<input type='text' name='jobid'/>\n";
     printf "<input type='submit' name='submit' value='Import now'/>\n";
     printf "</form>";
+    printf "</div>";
 
 end
