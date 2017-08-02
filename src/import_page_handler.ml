9e58432fc26b31e076263c8fe81628d7d9b5688d
Joshua Perrett <joshua.perrett@gmail.com>
2017-08-02 10:48:27 +0100

    possible fix to import page styling problems

--- src/import_page_handler.ml
+++ src/import_page_handler.ml
@@ -4,12 +4,12 @@ let t ~args = object (self)
   inherit Html_handler.t ~args
 
   method private write_body =
-    printf "<div class=\"import_page box\">\n";
+    printf "<div class=\"import_page\">\n";
     printf "<p>Data from completed XenRT jobs is imported automatically daily.</p>\n";
     printf "<p>This page can be used to force the immediate import of one or more XenRT jobs.</p>\n";
     printf "<form action='/' method='post'>\n";
     printf "<input type='hidden' name='p' value='import_jobs'/>\n";
-    printf "<div class=\"import_page input_label\">Job number: </div>"; (*newline omitted as html will render a space in between these elements*)
+    printf "<div class=\"import_page_label\">Job number: </div>"; (*newline omitted as html will render a space in between these elements*)
     printf "<input type='text' name='jobid'/>";
     printf "<input type='submit' name='submit' value='Import now'/>\n";
     printf "</form>";
