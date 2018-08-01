ee02b67f13c2d77665bcffd5b4895f4ff057d3fe
Joshua Perrett <joshua.perrett@gmail.com>
2018-08-01 18:34:21 +0100

    added c3 (broke d3)

--- static/rage.js
+++ static/rage.js
@@ -718,7 +718,8 @@ function GraphObject() {
     }
     $("#stop_plotting").prop("disabled", false);
     var start = new Date();
-    d3_graph(graph, series, options, o);
+    //d3_graph(graph, series, options, o);
+    c3_graph(series, options, o);
     flot_object = $.plot(graph, series, options, function() {
       console.log("Plotting took " + (new Date() - start) + "ms.");
       // click
