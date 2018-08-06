ff04a275ef581743a99836e701710bfa0f204cb2
Joshua Perrett <joshua.perrett@gmail.com>
2018-08-06 11:45:12 +0100

    c3: added average line

--- static/c3/c3_graph.js
+++ static/c3/c3_graph.js
@@ -3,13 +3,18 @@ $(function () {
 });
 
 function c3_graph(series, options, o) {
-	// keep only elements that contain useful information for the graph
+	// keep only the elements that hold the average points
+	mean_data = series.filter(function (x) {return ('lines' in x)});
+	// keep only elements that contain graph points (remove average points)
 	series = series.filter(function (x) {return !('lines' in x)});
 
 	var chart_data = {
         	xs: {},
-        	columns: []
+        	columns: [],
+		type: 'scatter',
+		types: {}
 	};
+	//add the necessary data to chart_data to plot graph points
 	series.forEach(function (item, index) {
 		chart_data.xs['data' + index] = 'x' + index;
 		var x_values = item.data.map(function(pair) {
@@ -21,7 +26,24 @@ function c3_graph(series, options, o) {
 		chart_data.columns.push(['x' + index].concat(x_values));
 		chart_data.columns.push(['data' + index].concat(y_values));
 	});
+	//add the necessary data to chart_data to plot average graph points
+	mean_data.forEach(function (item) {
+		chart_data.xs[item.tooltiplabel] = 'x_mean';
+		var x_values = item.data.map(function(pair) {
+			return pair[0];
+		});
+		var y_values = item.data.map(function(pair) {
+			return pair[1];
+		});
+		chart_data.columns.push(['x_mean'].concat(x_values));
+		chart_data.columns.push([item.tooltiplabel].concat(y_values));
+		//set type for plots of average points
+		chart_data.types[item.tooltiplabel] = 'line';
+	});
+
+	console.log('Options', o);
 	console.log("Chart Data:", chart_data);
+
 	var chart = c3.generate({
 		bindto: '#graph2',
 		data: chart_data,
